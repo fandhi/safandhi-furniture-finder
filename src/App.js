317ee0840359ca6776dc51cdc4ef8a91a5bc9de9
Safandhi Yonathan <safandhi.yonathan@elevenia.co.id>
2019-06-27 02:28:26 +0700

    update fetch filter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import { Card, Col, Container, Dropdown, Row, Spinner, Form } from 'react-bootstrap';
 
-// const API = "https://hn.algolia.com/api/v1/search?query="
 const API = "http://www.mocky.io/v2/5c9105cb330000112b649af8"
 const DEFAULT_QUERY = 'redux';
 
@@ -14,7 +13,7 @@ class App extends Component {
     super(props)
     this.state = {
       dataFurniture: [],
-      hits: [],
+      furnitureStyles: [],
       isLoading: false,
     }
   }
@@ -23,18 +22,36 @@ class App extends Component {
     this.setState({ isLoading: true });
 
     axios.get(API)
+      // .then(function (response) {
+      //   // handle success
+      //   console.log(response);
+      // })
       .then(result => this.setState({
-        hits: result.data.products,
+        furnitureStyles: result.data.furniture_styles,
         isLoading: false,
       }))
       .catch(error => this.setState({
         error,
         isLoading: false
       }));
+    axios.get(API)
+    .then(result => this.setState({
+      dataFurniture: result.data.products,
+      isLoading: false,
+    }))
+              .then(function (response) {
+                // handle success
+                console.log(response.data);
+              })
+      .catch(error => this.setState({
+        error,
+        isLoading: false
+      }));
+    // console.log(hits)
   }
 
   render() {
-    const { hits, isLoading } = this.state;
+    const { dataFurniture, furnitureStyles, isLoading } = this.state;
     if (isLoading) {
       return <div className="align">
         <Spinner animation="border" variant="info" className="align-center" />
@@ -44,26 +61,26 @@ class App extends Component {
       <div className="App">
         <Container className="content">
           <header className="bg-info p-3">
-
-            <Row>
-              <Col><h3 className="text-light">Searh Furniture</h3></Col>
-            </Row>
             <Form>
+              <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Control type="text" placeholder="Searh Furniture" />
+              </Form.Group>
               <Row>
                 <Col>
                   <Form.Group controlId="exampleForm.ControlInput1">
                     <Form.Control type="email" placeholder="name@example.com" />
                   </Form.Group>
+                  {furnitureStyles.map((item, index) =>
+                    <Form.Check inline label={item} type="checkbox" id={item} />
+                  )}
                 </Col>
 
                 <Col>
                   <Form.Group controlId="exampleForm.ControlSelect1">
                     <Form.Control as="select">
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                      <option>4</option>
-                      <option>5</option>
+                      {dataFurniture.map((item, index) =>
+                        <option>{item.delivery_time}</option>
+                      )}
                     </Form.Control>
                   </Form.Group>
                 </Col>
@@ -71,9 +88,9 @@ class App extends Component {
             </Form>
           </header>
           <Row className="row p-2">
-            {hits.map((item, index) =>
-              <Col md={6} sm={6} className="mb-4">
-                <Card key={index} className="shadow-sm">
+            {dataFurniture.map((item, index) =>
+              <Col md={6} sm={6} className="mb-4" key={index}>
+                <Card className="shadow-sm">
                   <Card.Body>
                     <Card.Title>{item.name}</Card.Title>
                     <Card.Text className="card-price text-warning position-absolute ">Rp {item.price.toLocaleString(undefined, { maximumFractionDigits: 2 })}</Card.Text>
